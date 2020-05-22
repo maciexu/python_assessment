eb249ecdd839888d11f302eeb67d07b873e7b6d2
maciexu <43970987+maciexu@users.noreply.github.com>
2020-05-22 19:42:51 -0400

    Create python_ass.py

---
+++ python_ass.py
@@ -0,0 +1,26 @@
+def make_dict(**kwargs):
+    return kwargs
+
+make_dict(a = 1, b = 2)
+
+
+
+x = (1, 2, 3)
+s = set(x)    # {}
+
+
+
+
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+m = Person('Michael')
+
+m.name
+
+
+
+
+
